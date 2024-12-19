cdb3a7261ad04fc3e3d5f57586d812e08b684262
Keenan Tims <ktims@gotroot.ca>
2024-12-18 20:56:27 -0800

    day19: pre puzzle boilerplate

---
+++ src/day19.rs
@@ -0,0 +1,31 @@
+use aoc_runner_derive::aoc;
+
+fn parse(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day19, part1)]
+fn part1(input: &str) -> i64 {
+    todo!()
+}
+
+#[aoc(day19, part2)]
+fn part2(input: &str) -> i64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const EXAMPLE: &str = "";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(EXAMPLE), 0);
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(EXAMPLE), 0);
+    }
+}
